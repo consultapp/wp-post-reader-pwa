c259bc04410f7f12c10443ba833f2d5e1aed672f
Dmitry Egorov <info@consultapp.ru>
2023-09-09 01:44:42 +0400

    1

--- vite.config.js
+++ vite.config.js
@@ -1,65 +1,65 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
-      strategies: "injectManifest",
-      srcDir: "src",
-      filename: "customSW.js",
+      registerType: 'autoUpdate',
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'customSW.js',
       // devOptions: {
       //   enabled: true,
       //   type: "module",
       // },
       manifest: {
-        name: "PWA | Wordpress posts reader",
-        short_name: "PWA WP post reader",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#000000",
-        lang: "ru",
-        scope: "/",
-        theme_color: "#ffffff",
-        orientation: "portrait",
+        name: 'PWA | Wordpress posts reader',
+        short_name: 'PWA WP post reader',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#000000',
+        lang: 'ru',
+        scope: '/',
+        theme_color: '#ffffff',
+        orientation: 'portrait',
         icons: [
           {
-            src: "/manifest-icon-192.maskable.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
+            src: '/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
           },
           {
-            src: "/manifest-icon-192.maskable.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
+            src: '/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
           },
           {
-            src: "/manifest-icon-512.maskable.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
+            src: '/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
           },
           {
-            src: "/manifest-icon-512.maskable.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
+            src: '/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
           },
         ],
       },
 
       workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
       },
       includeAssets: [
-        "favicon.ico",
-        "apple-touch-icon.png",
-        "safari-pinned-tab.svg",
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'safari-pinned-tab.svg',
       ],
     }),
   ],
-});
+})
