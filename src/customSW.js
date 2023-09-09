2c28c68deab9c71c3e0075825c8d375f836dd821
Dmitry Egorov <info@consultapp.ru>
2023-09-09 16:30:54 +0400

    start + icons update

--- src/customSW.js
+++ src/customSW.js
@@ -1,64 +1,59 @@
-import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
+import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
 
-import { Exception } from "sass";
-import initNotifications from "./serviceWorker/notification";
+import { Exception } from 'sass'
+import initNotifications from './serviceWorker/notification'
 
 // import { clientsClaim } from "workbox-core";
 // self.skipWaiting();
 // clientsClaim();
 
-cleanupOutdatedCaches();
-precacheAndRoute(self.__WB_MANIFEST);
+cleanupOutdatedCaches()
+precacheAndRoute(self.__WB_MANIFEST)
 
-const cacheVersion = "11";
-const staticCacheName = "s-app-" + cacheVersion;
-const dynamicCacheName = "d-app-" + cacheVersion;
+const cacheVersion = '11'
+const staticCacheName = 's-app-' + cacheVersion
+const dynamicCacheName = 'd-app-' + cacheVersion
 
-const assetUrls = [
-  "/index.html",
-  "/manifest.webmanifest",
-  "/customSW.js",
-  "/favicon.ico",
-];
+const assetUrls = []
 
-self.addEventListener("install", async (event) => {
+self.addEventListener('install', async (event) => {
   try {
     event.waitUntil(
       caches.open(staticCacheName).then((cache) => {
-        return cache.addAll(assetUrls);
+        return cache.addAll(assetUrls)
       })
-    );
+    )
   } catch (error) {
-    throw Exception("Open cache error");
+    throw Exception('Open cache error')
   }
-});
+})
 
-self.addEventListener("activate", async () => {
-  const cacheNames = await caches.keys();
+self.addEventListener('activate', async () => {
+  const cacheNames = await caches.keys()
   await Promise.all(
     cacheNames
       .filter((name) => name !== staticCacheName)
       .filter((name) => name !== dynamicCacheName)
       .map((name) => caches.delete(name))
-  );
-  initNotifications();
-});
+  )
+  initNotifications()
+})
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.open(dynamicCacheName).then((cache) => {
       return cache.match(event.request).then((cachedResponse) => {
         const fetchedResponse = fetch(event.request).then((networkResponse) => {
-          cache.put(event.request, networkResponse.clone());
+          cache.put(event.request, networkResponse.clone())
 
-          return networkResponse;
-        });
+          return networkResponse
+        })
 
-        return cachedResponse || fetchedResponse;
-      });
+        return cachedResponse || fetchedResponse
+      })
     })
-  );
-});
+  )
+})
 
 // self.addEventListener("fetch", (event) => {
 //   const { request } = event;
@@ -72,8 +67,8 @@ self.addEventListener("fetch", (event) => {
 // });
 
 async function cacheFirst(request) {
-  const cached = await caches.match(request);
-  return cached ?? (await fetch(request));
+  const cached = await caches.match(request)
+  return cached ?? (await fetch(request))
 }
 
 // async function messageAllClients(data, openNewClient = false) {
--- vite.config.js
+++ vite.config.js
@@ -26,6 +26,16 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
             src: '/manifest-icon-192.maskable.png',
             sizes: '192x192',
             type: 'image/png',
@@ -55,11 +65,11 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
       },
-      includeAssets: [
-        'favicon.ico',
-        'apple-touch-icon.png',
-        'safari-pinned-tab.svg',
-      ],
+      // includeAssets: [
+      //   'favicon.ico',
+      //   'apple-touch-icon.png',
+      //   'safari-pinned-tab.svg',
+      // ],
     }),
   ],
 })
