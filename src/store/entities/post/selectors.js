016b16b1c01ef2bb0be1fc89b308d2b54d9661fe
Dmitry Egorov <info@consultapp.ru>
2023-09-18 01:18:12 +0300

    selectors update ro createSelector

--- src/store/entities/post/selectors.js
+++ src/store/entities/post/selectors.js
@@ -1,52 +1,95 @@
+import { createSelector } from "@reduxjs/toolkit";
 import { LOADING_STATUS } from "../../../constants";
 
 export const selectPostModule = (state) => state.post;
+const selectPostId = (_, { postId }) => postId;
+const selectPageIndex = (_, { pageIndex = 1 }) => pageIndex;
+// const selectSlug = (_, { slug }) => slug;
 
 // ID
-export const selectPostIds = (state) => selectPostModule(state).ids;
-export const selectPostById = (state, { postId }) =>
-  selectPostModule(state).entities[postId];
-export const selectIfPostAlreadyLoaded = (state, { postId }) =>
-  selectPostIds(state).includes(postId);
+export const selectPostIds = createSelector(
+  [selectPostModule],
+  (post) => post.ids
+);
+// export const selectPostIds = (state) => selectPostModule(state).ids;
+export const selectPostById = createSelector(
+  [selectPostModule, selectPostId],
+  (post, postId) => post.entities[postId]
+);
+// export const selectPostById = (state, { postId }) =>
+//   selectPostModule(state).entities[postId];
+
+export const selectIfPostAlreadyLoaded = createSelector(
+  [selectPostIds, selectPostId],
+  (post, postId) => post.includes(postId)
+);
+// export const selectIfPostAlreadyLoaded = (state, { postId }) =>
+//   selectPostIds(state).includes(postId);
 
 // Slug
-export const selectPostSlugToId = (state) =>
-  selectPostModule(state).slugToId || {};
+export const selectPostSlugToId = createSelector(
+  [selectPostModule],
+  (post) => post.slugToId || {}
+);
+
 export const selectPostBySlug = (state, { slug }) =>
   selectPostById(state, {
     postId: selectPostSlugToId(state)[encodeURI(slug).toLowerCase()],
   });
 
 // Status
-export const selectPostLoadingStatus = (state) =>
-  selectPostModule(state).loadingStatus;
-export const selectIsPostLoading = (state) =>
-  selectPostLoadingStatus(state) === LOADING_STATUS.pending;
-export const selectIsPostRejected = (state) =>
-  selectPostLoadingStatus(state) === LOADING_STATUS.rejected;
+export const selectPostLoadingStatus = createSelector(
+  [selectPostModule],
+  (post) => post.loadingStatus
+);
 
-export const selectIsPostStatus404 = (state) =>
-  selectPostModule(state).status404;
+export const selectIsPostLoading = createSelector(
+  [selectPostLoadingStatus],
+  (status) => status === LOADING_STATUS.pending
+);
 
-export const selectIsPostNotFound = (state) =>
-  selectPostLoadingStatus(state) === LOADING_STATUS.rejected &&
-  selectIsPostStatus404(state);
+export const selectIsPostRejected = createSelector(
+  [selectPostLoadingStatus],
+  (status) => status === LOADING_STATUS.rejected
+);
+
+export const selectIsPostStatus404 = createSelector(
+  [selectPostModule],
+  (post) => post.status404
+);
+
+export const selectIsPostNotFound = createSelector(
+  [selectPostLoadingStatus, selectIsPostStatus404],
+  (a, b) => a === LOADING_STATUS.rejected && b
+);
 
 // pageIndex + Page
-export const selectPostsByPageIndex = (state, { pageIndex = 1 }) =>
-  Object.values(selectPostModule(state).entities || {}).filter(
-    (item) => item?.pageIndex === Number(pageIndex)
-  );
-export const selectPostsIdsByPageIndex = (state, { pageIndex = 1 }) =>
-  Object.values(selectPostModule(state).entities || {}).reduce((acc, item) => {
-    if (item?.pageIndex === Number(pageIndex)) {
-      acc.push(item.id);
-    }
-    return acc;
-  }, []);
 
-export const selectLoadedPostsPages = (state) =>
-  selectPostModule(state).loadedPages;
+export const selectPostsByPageIndex = createSelector(
+  [selectPostModule, selectPageIndex],
+  (post, pageIndex) =>
+    Object.values(post.entities || {}).filter(
+      (item) => item?.pageIndex === Number(pageIndex)
+    )
+);
 
-export const selectIsPostPageLoaded = (state, { pageIndex }) =>
-  selectLoadedPostsPages(state).includes(Number(pageIndex));
+export const selectPostsIdsByPageIndex = createSelector(
+  [selectPostModule, selectPageIndex],
+  (post, pageIndex) =>
+    Object.values(post.entities || {}).reduce((acc, item) => {
+      if (item?.pageIndex === Number(pageIndex)) {
+        acc.push(item.id);
+      }
+      return acc;
+    }, [])
+);
+
+export const selectLoadedPostsPages = createSelector(
+  [selectPostModule],
+  (post) => post.loadedPages
+);
+
+export const selectIsPostPageLoaded = createSelector(
+  [selectLoadedPostsPages, selectPageIndex],
+  (items, pageIndex) => items.includes(Number(pageIndex))
+);
